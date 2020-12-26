8ca451324f910f459a853cdd43c791364606441a
Julian Poyourow <julianpoyo@gmail.com>
2020-12-26 10:51:22 -0800

    Fix for selfhost recipe clipping

--- Backend/src/routes/clip.js
+++ Backend/src/routes/clip.js
@@ -34,7 +34,7 @@ const clipRecipe = async clipUrl => {
     throw err;
   }
 
-  await page.evaluate(`() => {
+  await page.evaluate(`
     try {
       // Force lazyload for content listening to scroll
       window.scrollTo(0, document.body.scrollHeight);
@@ -42,12 +42,10 @@ const clipRecipe = async clipUrl => {
       window.define = null;
       window.exports = null;
     } catch(e) {}
-  }`);
+  `);
 
   await page.addScriptTag({ path: './node_modules/@julianpoy/recipe-clipper/dist/recipe-clipper.umd.js' });
-  const recipeData = await page.evaluate(() => {
-    return window.RecipeClipper.clipRecipe();
-  });
+  const recipeData = await page.evaluate(`window.RecipeClipper.clipRecipe()`);
 
   console.log(JSON.stringify(recipeData));
 
